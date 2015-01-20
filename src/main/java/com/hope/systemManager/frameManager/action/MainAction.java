8333a639c7ffcc969be0d259b87fed4c106cb5e1
bibo <bibo@digitalchina.com>
2015-01-20 11:25:52 +0800

    frame base

--- src/main/java/com/hope/systemManager/frameManager/action/MainAction.java
+++ src/main/java/com/hope/systemManager/frameManager/action/MainAction.java
@@ -35,87 +35,84 @@ import com.hope.systemManager.frameManager.form.TabViewForm;
 import com.hope.systemManager.functionManager.model.SysFunction;
 import com.hope.systemManager.functionManager.service.SysFunctionService;
 
-public class MainAction implements Serializable{
+public class MainAction implements Serializable {
 	HttpServletRequest request = (HttpServletRequest) FacesContext
 			.getCurrentInstance().getExternalContext().getRequest();
 	HttpSession httpSession = request.getSession();
-	
-	public MainAction(){
+
+	public MainAction() {
 	}
-	
+
 	@PostConstruct
-	public void init(){
+	public void init() {
 		initPanelMenu();
 	}
-	
-	public void initPanelMenu(){
-		panelMenu =new DefaultMenuModel();
-		List<SysFunction> list=sysFunctionService.sysFunctionQueryAll();
-		
-		DefaultSubMenu subMenu=null;
-		DefaultSubMenu subMenu1=null;
-		DefaultMenuItem menuItem1=null;
-		DefaultMenuItem menuItem2=null;
-		
-		int addMenuBefore=0;
-		int addmenuAfter=0;
-		for(SysFunction sf:list){
-			if(sf.getLevelGrade()==1){
-				subMenu=new DefaultSubMenu();
+
+	public void initPanelMenu() {
+		panelMenu = new DefaultMenuModel();
+		List<SysFunction> list = sysFunctionService.sysFunctionQueryAll();
+
+		DefaultSubMenu subMenu = null;
+		DefaultSubMenu subMenu1 = null;
+		DefaultMenuItem menuItem1 = null;
+		DefaultMenuItem menuItem2 = null;
+
+		int addMenuBefore = 0;
+		int addmenuAfter = 0;
+		for (SysFunction sf : list) {
+			if (sf.getLevelGrade() == 1) {
+				subMenu = new DefaultSubMenu();
 				subMenu.setLabel(sf.getSysFunName());
-				//subMenu.setStyle("font-size:15px;");
 				subMenu.setStyleClass("ct-fontsize");
 				addmenuAfter++;
-			}else if (sf.getLevelGrade()==2) {
-				if(null==sf.getUrl()||sf.getUrl().equals("")){
-					subMenu1=new DefaultSubMenu();
+			} else if (sf.getLevelGrade() == 2) {
+				if (null == sf.getUrl() || sf.getUrl().equals("")) {
+					subMenu1 = new DefaultSubMenu();
 					subMenu1.setLabel(sf.getSysFunName());
-					//subMenu1.setStyle("font-size:15px;");
 					subMenu1.setStyleClass("ct-fontsize");
 					subMenu.addElement(subMenu1);
-				}else {
-					menuItem1=new DefaultMenuItem();
+				} else {
+					menuItem1 = new DefaultMenuItem();
 					menuItem1.setValue(sf.getSysFunName());
-					menuItem1.setCommand("#{MainAction.addTab('"+menuItem1.getValue()+"','"+sf.getUrl()+"')}");
-					//menuItem1.setUrl(sf.getUrl());
+					menuItem1.setCommand("#{mainAction.addTab('"
+							+ menuItem1.getValue() + "','" + sf.getUrl()
+							+ "')}");
 					menuItem1.setUpdate(":form:tabView");
-					//menuItem1.setStyle("font-size:15px;");
 					menuItem1.setStyleClass("ct-fontsize");
 					menuItem2.setAjax(true);
 					subMenu.addElement(menuItem1);
 				}
-			}else if (sf.getLevelGrade()==3) {
-				menuItem2=new DefaultMenuItem();
+			} else if (sf.getLevelGrade() == 3) {
+				menuItem2 = new DefaultMenuItem();
 				menuItem2.setValue(sf.getSysFunName());
-				//menuItem2.setUrl(sf.getUrl());
 				menuItem2.setAjax(true);
-				menuItem2.setCommand("#{MainAction.addTab('"+menuItem2.getValue()+"','"+sf.getUrl()+"')}");
+				menuItem2.setCommand("#{mainAction.addTab('"
+						+ menuItem2.getValue() + "','" + sf.getUrl() + "')}");
 				menuItem2.setUpdate(":tabView");
-				//menuItem2.setStyle("font-size:15px;");
 				menuItem2.setStyleClass("ct-fontsize");
 				subMenu1.addElement(menuItem2);
 			}
-			if(addMenuBefore!=addmenuAfter){
+			if (addMenuBefore != addmenuAfter) {
 				panelMenu.getElements().add(subMenu);
 				addMenuBefore++;
 			}
 		}
 
 		tabs = new ArrayList<TabViewForm>();
-//		TabViewForm tabViewForm=new TabViewForm();
-//		tabViewForm.setTitle("ceshi");
-//		tabViewForm.setUrl("/systemManager/userManager/user_query.xhtml");
-//		tabs.add(tabViewForm);
+		// TabViewForm tabViewForm=new TabViewForm();
+		// tabViewForm.setTitle("ceshi");
+		// tabViewForm.setUrl("/systemManager/userManager/user_query.xhtml");
+		// tabs.add(tabViewForm);
 	}
-	
+
 	private List<TabViewForm> tabs;
-	
+
 	public List<TabViewForm> getTabs() {
 		return tabs;
 	}
 
 	private SysFunctionService sysFunctionService;
-	
+
 	private MenuModel panelMenu;
 	private TabView tabView;
 	private int tabIdTemp;
@@ -143,7 +140,7 @@ public class MainAction implements Serializable{
 	public void setPanelMenu(MenuModel panelMenu) {
 		this.panelMenu = panelMenu;
 	}
-	
+
 	public SysFunctionService getSysFunctionService() {
 		return sysFunctionService;
 	}
@@ -151,36 +148,33 @@ public class MainAction implements Serializable{
 	public void setSysFunctionService(SysFunctionService sysFunctionService) {
 		this.sysFunctionService = sysFunctionService;
 	}
-	
-	public void addTab(String value,String url){
-		tabIdTemp=tabs.size();
-		for(TabViewForm tv:tabs){
-        	if(tv.getTitle().equals(value)){
-        		return;
-        	}
-        }
-		TabViewForm tabViewForm=new TabViewForm();
+
+	public void addTab(String value, String url) {
+		tabIdTemp = tabs.size();
+		for (TabViewForm tv : tabs) {
+			if (tv.getTitle().equals(value)) {
+				return;
+			}
+		}
+		TabViewForm tabViewForm = new TabViewForm();
 		tabViewForm.setTitle(value);
 		tabViewForm.setUrl(url);
 		tabs.add(tabViewForm);
 	}
 
 	public void onTabChange(TabChangeEvent event) {
-//		FacesMessage msg = new FacesMessage("Tab Change", "Change tab: " + event.getTab().getTitle());
-//        FacesContext.getCurrentInstance().addMessage(null, msg);
-    }
-	
-	public void onTabClose(TabCloseEvent event){
-//		FacesMessage msg = new FacesMessage("Tab Closed", "Closed tab: " + event.getTab().getTitle());
-//        FacesContext.getCurrentInstance().addMessage(null, msg);
-        for(TabViewForm tv:tabs){
-        	if(tv.getTitle().equals(event.getTab().getTitle())){
-        		tabs.remove(tv);
-        	}
-        }
+
 	}
-	
-	public String logout(){
+
+	public void onTabClose(TabCloseEvent event) {
+		for (TabViewForm tv : tabs) {
+			if (tv.getTitle().equals(event.getTab().getTitle())) {
+				tabs.remove(tv);
+			}
+		}
+	}
+
+	public String logout() {
 		System.out.println("系统退出");
 		httpSession.invalidate();
 		return "logout";
--- src/main/java/com/hope/systemManager/userManager/action/UserAction.java
+++ src/main/java/com/hope/systemManager/userManager/action/UserAction.java
@@ -1,48 +1,32 @@
 package com.hope.systemManager.userManager.action;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.ViewScoped;
 
 import org.primefaces.event.RowEditEvent;
 
-import com.hope.systemManager.frameManager.action.Car;
 import com.hope.systemManager.userManager.model.User;
 import com.hope.systemManager.userManager.service.UserService;
 import com.hope.util.Tools;
 
-public class UserAction implements Serializable{
-	
+public class UserAction implements Serializable {
+
 	@PostConstruct
-	public void init(){
-		System.out.println("用户查询初始化");
+	public void init() {
 		initUserList();
-		
 	}
-	
-	public void initUserList(){
-		users=userService.userQueryAll();
-		
-//		 List<User> list = new ArrayList<User>();
-//	        for(int i = 0 ; i < 10 ; i++) {
-//	        	User user=new User();
-//	        	user.setUsercode("a"+i);
-//	            list.add(user);
-//	        }
-//	        users=list;
+
+	public void initUserList() {
+		users = userService.userQueryAll();
 	}
-	
+
 	private UserService userService;
 	private List<User> users;
 	private List<User> filteredUsers;
 	private String userCode;
-	
-	
+
 	public String getUserCode() {
 		return userCode;
 	}
@@ -50,7 +34,7 @@ public class UserAction implements Serializable{
 	public void setUserCode(String userCode) {
 		this.userCode = userCode;
 	}
-	
+
 	public List<User> getFilteredUsers() {
 		return filteredUsers;
 	}
@@ -66,32 +50,31 @@ public class UserAction implements Serializable{
 	public void setUserService(UserService userService) {
 		this.userService = userService;
 	}
-	
+
 	public List<User> getUsers() {
 		return users;
 	}
-	
-	public void userFuzzyQuery(){
-		User user=new User();
+
+	public void userFuzzyQuery() {
+		User user = new User();
 		user.setUsercode(Tools.SpaceDisappear(this.userCode));
-		users=userService.userFuzzyQuery(user);
-		System.out.println("模糊查询用户"+users.size());
+		users = userService.userFuzzyQuery(user);
 	}
-	
+
 	public void onRowEdit(RowEditEvent event) {
-		User user=(User)event.getObject();
+		User user = (User) event.getObject();
 		try {
 			userService.update(user);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		initUserList();
-    }
-     
-    public void onRowCancel(RowEditEvent event) {
-        //FacesMessage msg = new FacesMessage("Edit Cancelled", ((Car) event.getObject()).getId());
-        //FacesContext.getCurrentInstance().addMessage(null, msg);
-    }
 
+		initUserList();
+	}
+
+	public void onRowCancel(RowEditEvent event) {
+		// FacesMessage msg = new FacesMessage("Edit Cancelled", ((Car)
+		// event.getObject()).getId());
+		// FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
 }
